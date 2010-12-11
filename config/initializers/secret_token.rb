d4219a41740c7ce5057df9db028fb45abeb68e76
Khaled Talaat <ktlt87@gmail.com>
2010-12-11 20:14:36 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Egypon::Application.config.secret_token = '078a59cf9c848e43297ffc68da99f01459ed888796cbe365a9dbfb308b875118dd71b84a7bd3fce22470b845f74850c5a620722e45c028ecb12152bf50160f1c'
